64bb373bf1211eb69223d0eefd43cba2e81ebd79
IMRAN_stack53 <imranshar122@gmail.com>
2024-08-31 11:33:15 -0700

    LAB1

---
+++ imran/DSAPERCTICE/Lab1.java
@@ -0,0 +1,16 @@
+public class Lab1 {
+
+    public static void main(String[] args){ 
+            int [] nums;
+            nums = new int[6];
+            nums[0]=5;
+            nums[1]=15;
+            nums[2]=25;
+            nums[3]=35;
+            nums[4]=45;
+            nums[5]=55;
+       for(int i=0;i<=6;i++){
+           System.out.println("Element  at  index " + i +" :" + nums[i]);
+       }
+    }
+}

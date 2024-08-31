0c9cc013d6f18b0a4e25ed34112b98188ccb3268
IMRAN_stack53 <imranshar122@gmail.com>
2024-08-31 06:00:24 -0700

    myfile

---
+++ fahad.java
@@ -0,0 +1,11 @@
+public class fahad {
+
+
+        public static void main(String[] args) {
+            
+            System.out.println("hello world");
+            System.out.println("this  is imran shar");
+        }
+        
+        }
+        
